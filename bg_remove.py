58c67de9fb515531c8c280958833048eb95ce130
joaorm <joaorochamelo@gmail.com>
2025-02-26 21:51:35 +0000

    I chnaged the title

--- bg_remove.py
+++ bg_remove.py
@@ -7,7 +7,7 @@ import base64
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 
-st.write("## REMOVE BACKGROUND OF IMAGES OF ANIMALS")
+st.write("## NOVA SBE CLASS - Background Images")
 st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
