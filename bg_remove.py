641b87c87a9025e29c83491e46027e06970bf5cf
joaorm <joaorochamelo@gmail.com>
2026-04-18 12:43:56 +0100

    Titulo Modificado 18/abr

--- bg_remove.py
+++ bg_remove.py
@@ -1,6 +1,6 @@
 #############################
 #
-#           DEV
+#           DEV -> run using "streamlit run bg_remove.py"
 #
 #############################
 
@@ -13,7 +13,7 @@ import base64
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 
-st.write("## FEATURE 2 - Background Images: Zebra")
+st.write("## IMAGE BACKGROUND REMOVER AULA 18/ABRIL")
 st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
